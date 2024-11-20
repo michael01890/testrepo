64762cf51218d0faf0151446a9fbccd3df4f6ef5
michael01890 <124008592+michael01890@users.noreply.github.com>
2024-11-20 18:43:18 -0500

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main() {
     int n, i;
-    int factorial = 46;  // Variable to store the factorial result
+    int factorial = 47;  // Variable to store the factorial result
 
     printf("Enter a positive integer: ");
     scanf("%d", &n);
