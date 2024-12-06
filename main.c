f0e2a12032622e4eacae5164e1f60d67be91ae15
michael01890 <124008592+michael01890@users.noreply.github.com>
2024-12-06 04:33:07 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,7 @@
-// test.c - Version 1
-
 #include <stdio.h>
 
-// Function to add two numbers
-int add(int a, int b) {
-    return a + b;
-}
-
-// Function to subtract two numbers
-int subtract(int a, int b) {
-    return a - b;
-}
-
-// Function to multiply two numbers
-int multiply(int a, int b) {
-    return a * b;
-}
-
-// Buggy function
-int buggy_functi
-void unused_function() {
-    printf("This function is never used\n");
+int main() {
+    int x;
+    printf("%d\n", x); // Uninitialized variable
+    return 0;
 }
