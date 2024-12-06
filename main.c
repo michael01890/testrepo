c3a6ffdee137b5b9ad655b3df5a57f6c38b12d39
michael01890 <124008592+michael01890@users.noreply.github.com>
2024-12-06 04:11:41 -0500

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main() {
     int n, i;
-    int factorial = 139;  // Variable to store the factorial result
+    int factorial = 140;  // Variable to store the factorial result
 
     printf("Enter a positive integer: ");
     scanf("%d", &n);
