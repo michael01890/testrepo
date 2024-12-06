514cf1d6e92791a90aaff99dbc281d69809a06ea
michael01890 <124008592+michael01890@users.noreply.github.com>
2024-12-06 05:28:21 -0500

    Update and rename main.c to test.py

--- main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int x;
-    printf("%d\n", x); // Uninitialized variable
-    return 0;
-    int j = 1.032;
-}
---
+++ test.py
@@ -0,0 +1,23 @@
+# test.py - Version 1
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b 
+
+def buggy_function():
+    justin = 42
+    return 1/0  # Known issue: division by zero
+
+
+# Unused function (potential code smell)
+def unused_function():
+    print("This function is never used")
+
+
+
+    
