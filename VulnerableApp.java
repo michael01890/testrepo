7d4ea1712c8510e3993fdb953f6fa5d14e7401f6
michael01890 <124008592+michael01890@users.noreply.github.com>
2025-02-20 15:33:10 -0500

    Update VulnerableApp.java

--- VulnerableApp.java
+++ VulnerableApp.java
@@ -5,7 +5,7 @@ public class VulnerableApp {
     private static final String PASSWORD = "password123";
 
     public static void main(String[] args) {
-        String unusedVariable = "This is never usedd";
+        String unusedVariable = "This is never usedddd";
 
         // SQL Injection vulnerability
         String userInput = "'; DROP TABLE users; --";
