f2a2e4f34bc82c313a6c0b5e85ef93c906ddeb69
michael01890 <124008592+michael01890@users.noreply.github.com>
2025-02-20 15:11:54 -0500

    Update file1.c

--- file1.c
+++ file1.c
@@ -3,7 +3,7 @@
 int main() {
     int num1, num2;       // Declare integer variables for user input
     int sum;              // Declare integer variable for the sum
-    int unusedVariable = 22;  // Unused integer variable
+    int unusedVariable = 23;  // Unused integer variable
 
     printf("Enter first number: ");
     scanf("%d", &num1);
